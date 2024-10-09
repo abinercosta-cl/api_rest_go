c999718f5ee2e52a513775b7a6189001d3d78383
abinercosta-cl <abinerclcosta@gmail.com>
2024-10-09 20:58:28 -0300

    created the function that initializes the productRepository struct

---
+++ repository/product_repository.go
@@ -0,0 +1,13 @@
+package repository
+
+import "database/sql"
+
+type ProductRepository struct {
+	connection *sql.DB
+}
+
+func NewProductRepository(connection *sql.DB) ProductRepository {
+	return ProductRepository{
+		connection: connection,
+	}
+}
