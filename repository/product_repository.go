aaecbab11abbe7715a46e86b16672c1f2bdab1af
abinercosta-cl <abinerclcosta@gmail.com>
2024-10-10 15:19:42 -0300

    create iteration the database

--- repository/product_repository.go
+++ repository/product_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"api_go_rest/model"
 	"database/sql"
+	"fmt"
 )
 
 type ProductRepository struct {
@@ -15,4 +16,31 @@ func NewProductRepository(connection *sql.DB) ProductRepository {
 	}
 }
 
-func (pr *ProductRepository) GetProducts() []model.Product
+func (pr *ProductRepository) GetProducts() ([]model.Product, error) {
+
+	query := "SELECT id, product_name, price FROM product"
+	rows, err := pr.connection.Query(query)
+	if err != nil {
+		fmt.Println(err)
+		return []model.Product{}, err
+	}
+	var productList []model.Product
+	var productObj model.Product
+
+	for rows.Next() {
+		err = rows.Scan(
+			&productObj.ID,
+			&productObj.Name,
+			&productObj.Price)
+
+		if err != nil {
+			fmt.Println(err)
+			return []model.Product{}, err
+		}
+
+		productList = append(productList, productObj)
+	}
+	rows.Close()
+
+	return productList, nil
+}
